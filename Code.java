09ddea0bf6f29a0db33a6c21b96ddbc71a5c2075
Priangshu das <daspriangshu99@gmail.com>
2024-08-10 03:18:16 +0530

    patterns day 2

--- Code.java
+++ Code.java
@@ -45,13 +45,74 @@ public class Code {
         }
     }
 
+    static void pattern5(int n) {
+        System.out.println("Pattern 5");
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            for (j = 0; j < n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6(int n) {
+        System.out.println("Pattern 6");
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n) {
+        System.out.println("Pattern 7");
+        int i, j, k, l;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            for (l = 1; l <= n - i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern8(int n) {
+        System.out.println("Pattern 8");
+        int i, j, k, l;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= 2 * n - (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            for (l = 1; l < i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
             // pattern1(n);
             // pattern2(n);
             // pattern3(n);
-            pattern4(n);
+            // pattern4(n);
+            // pattern5(n);
+            // pattern6(n);
+            // pattern7(n);
+            pattern8(n);
         }
     }
 }
