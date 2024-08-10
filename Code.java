85135079bd5cbce872c220e7427d7d79ef544162
Priangshu das <daspriangshu99@gmail.com>
2024-08-11 00:17:43 +0530

    patterns day 2

--- Code.java
+++ Code.java
@@ -88,16 +88,90 @@ public class Code {
     static void pattern8(int n) {
         System.out.println("Pattern 8");
         int i, j, k, l;
-        for (i = 1; i <= n; i++) {
-            for (j = 1; j < i; j++) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (k = 1; k <= 2 * n - (2 * i + 1); k++) {
+            for (k = 0; k < 2 * n - (2 * i + 1); k++) {
                 System.out.print("*");
             }
-            for (l = 1; l < i; l++) {
+            for (l = 0; l < i; l++) {
                 System.out.print(" ");
             }
+            System.out.println();
+        }
+    }
+
+    static void pattern9(int n) {
+        System.out.println("Pattern 9");
+        int i, j, k, l;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= i * 2 - 1; k++) {
+                System.out.print("*");
+            }
+            for (l = 1; l <= n - i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (k = 0; k < 2 * n - (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            for (l = 0; l < i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern10(int n) {
+        System.out.println("Pattern 10");
+        int i, j, k;
+        for (i = 1; i <= 2 * n - 1; i++) {
+            k = i;
+            if (i > n) {
+                k = 2 * n - i;
+            }
+            for (j = 1; j <= k; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern11(int n) {
+        System.out.println("Pattern 11");
+        int start = 1;
+
+        // Outer loop for the no. of rows
+        for (int i = 0; i < n; i++) {
+
+            // if the row index is even then 1 is printed first
+            // in that row.
+            if (i % 2 == 0)
+                start = 1;
+
+            // if odd, then the first 0 will be printed in that row.
+            else
+                start = 0;
+
+            // We alternatively print 1's and 0's in each row by using
+            // the inner for loop.
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start;
+            }
+
+            // As soon as the numbers for each iteration are printed, we move to the
+            // next row and give a line break otherwise all numbers
+            // would get printed in 1 line.
             System.out.println();
         }
     }
@@ -112,7 +186,10 @@ public class Code {
             // pattern5(n);
             // pattern6(n);
             // pattern7(n);
-            pattern8(n);
+            // pattern8(n);
+            // pattern9(n);
+            // pattern10(n);
+            pattern11(n);
         }
     }
 }
